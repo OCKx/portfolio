f7f16a791337332a0c60c35a9f52a39ebe7a1f6f
AG1559 <ag1559@student.jamk.fi>
2025-10-17 13:14:26 +0300

    v 2

---
+++ src/scripts.js
@@ -0,0 +1,6 @@
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// experience
+const numberOfYears = new Date().getFullYear() - 2022;
+document.getElementById("experience").textContent = numberOfYears;
